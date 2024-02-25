b8994c066e545464ef5ba5867048f12dba619b39
m4rs <marcel@marcelclaramunt.cat>
2024-02-25 19:55:59 +0100

    slideshow

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 > Simple, programmatic `framer-motion` animations
 
-## Loader
+## Loader/spinner
 
 > Fully controlled loader/spinner
 
 ![Loader in action](media/loader.gif)
 
-```typescript
+```jsx
 import { useLoader } from 'framer-animations'
 
 const { loader, animate } = useLoader()
@@ -31,5 +31,45 @@ return (
     // ...
     {loader}
   </div>
+)
+```
+
+## Slideshow/full-page carousel
+
+> Presence-based slideshow
+
+![Slideshow in action](media/slideshow.gif)
+
+```jsx
+import { Slideshow } from 'framer-animations'
+
+const [page, setPage] = useState('home')
+const [dir, setDir] = useState('right')
+
+const elem = (() => {
+  switch (page) {
+    case 'home':
+      return (
+        <div>
+          // ...
+          <button onClick={() => { setPage('about'); setDir('right') }}>About</button>
+        </div>
+      )
+    case 'about':
+      return (
+        <div>
+          // ...
+          <button onClick={() => { setPage('home'); setDir('left') }}>Back</button>
+        </div>
+      )
+    case '...':
+      ...
+  }
+})()
+
+return (
+  <Slideshow pageKey={page} direction={dir}>
+    {elem}
+  </Slideshow>
 )
 ```
--- src/Slideshow.tsx
+++ src/Slideshow.tsx
@@ -1,45 +1,40 @@
-import React from 'react';
-import { AnimatePresence, type Variant, motion, Variants } from "framer-motion"
-import { Key, ReactNode } from "react"
+import React, { Key, ReactNode } from "react"
+import { AnimatePresence, type Variant, motion, Variants, Transition } from "framer-motion"
 
-export type Direction = 'up' | 'down' | 'left' | 'right'
+export const directions = ['up', 'down', 'left', 'right'] as const
+export type Direction = typeof directions[number]
 
-type Custom = {
-  direction: Direction
-  back?: boolean
-}
-
-const variant = ({ direction, back }: Custom): Variant =>
+const variant = (direction: Direction, back?: boolean): Variant =>
   direction === 'down' ? { y: back ? '-100%' : '100%', x: 0 } :
   direction === 'up' ? { y: back ? '100%' : '-100%', x: 0 } :
   direction === 'right' ? { x: back ? '-100%' : '100%', y: 0 } :
   { x: back ? '100%' : '-100%', y: 0 }
 
 const variants: Variants = {
-  exit: ({direction, back}: Custom) => ({
-    ...variant({direction, back: !back}), opacity: 0
+  exit: (dir: Direction) => ({
+    ...variant(dir, true), opacity: 0
   }),
   center: {
     x: 0, y: 0, opacity: 1
   },
-  enter: (custom: Custom) => ({
-    ...variant(custom), opacity: 0
+  enter: (dir: Direction) => ({
+    ...variant(dir), opacity: 0
   })
 }
 
-export type Props = {
+type Props = {
   children: ReactNode
   pageKey: Key
   direction?: Direction
-  back?: boolean
-  duration?: number
+  /** `framer-motion` transition ([docs](https://www.framer.com/motion/transition/)). Default: `{ duration: 0.2 }` */
+  transition?: Transition
 }
-export function Slideshow({ children, pageKey, direction, back, duration }: Props) {
-  const custom: Custom = { direction: direction ?? 'right', back}
+export function Slideshow({ children, pageKey, direction, transition }: Props) {
+  const dir: Direction = direction ?? 'right'
   return (
-    <AnimatePresence initial={false} custom={custom}>
-      <motion.div key={pageKey} custom={custom} variants={variants}
-        initial='enter' animate='center' exit='exit' transition={{ duration: duration ?? 0.2 }}
+    <AnimatePresence initial={false} custom={dir}>
+      <motion.div key={pageKey} custom={dir} variants={variants}
+        initial='enter' animate='center' exit='exit' transition={transition ?? {duration: 0.2}}
         style={{ width: '100%', height: '100%', position: 'absolute' }}
       >
         {children}
@@ -47,3 +42,5 @@ export function Slideshow({ children, pageKey, direction, back, duration }: Prop
     </AnimatePresence>
   )
 }
+
+export default Slideshow
