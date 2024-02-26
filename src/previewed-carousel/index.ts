d5b16a4c069b233a0504637c6dff4213e04253b1
m4rs <marcel@marcelclaramunt.cat>
2024-02-26 20:03:29 +0100

    newver

--- src/previewed-carousel/index.ts
+++ src/previewed-carousel/index.ts
@@ -1,2 +1,2 @@
-export { usePreviewedCarousel, type Config, type Hook, type Items } from './usePreviewedCarousel'
+export { usePreviewedCarousel, type Config, type Hook, type ItemProps } from './usePreviewedCarousel'
 export { PreviewedCarousel, type Item, type PreviewConfig } from './PreviewedCarousel'
