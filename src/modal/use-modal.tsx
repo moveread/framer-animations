cd8002a37f6dc24a3a3503c48f0472e1d38090ea
m4rs <marcel@marcelclaramunt.cat>
2024-02-27 06:46:38 +0100

    simplified modal hook

--- src/modal/use-modal.tsx
+++ src/modal/use-modal.tsx
@@ -1,46 +1,14 @@
-import React, { memo } from "react";
-import { type MotionProps, motion, Variant, useAnimation } from "framer-motion";
+import React, { useState } from "react";
+import { type MotionProps } from "framer-motion";
+import { Modal } from "./Modal";
 
-export type Hook = {
-  Modal: typeof motion.div
-  animate(action: 'show' | 'hide'): Promise<void>
+type Hook = {
+  Modal: JSX.Element
+  animate(show: boolean): void
 }
-
-export type Config = {
-  zIndex?: number
-  hiddenZIndex?: number
-  opacity?: number
-}
-const defaultCfg: Required<Config> = { zIndex: 1, hiddenZIndex: -1, opacity: 0.7 }
-
-/** Opacity-based, controlled modal (always mounted) */
-export function useModal(config?: Config): Hook {
-  const { zIndex, hiddenZIndex, opacity } = {...defaultCfg, ...config}
-
-  const controls = useAnimation()
-
-  const variants: Record<string, Variant> = {
-    hide: {
-      opacity: 0,
-      zIndex: hiddenZIndex
-    },
-    show: {
-      opacity, zIndex
-    }
-  }
-
-  const animate: Hook['animate'] = async (action) => {
-    controls.stop()
-    await controls.start(action)
-  } 
-
-  const Modal = memo(({ style, ...props }: MotionProps) => (
-    <motion.div initial='hide' animate={controls} variants={variants} style={{
-      position: 'absolute', top: 0, left: 0, width: '100%', height: '100%',
-      background: '#444', display: 'flex', alignItems: 'center', justifyContent: 'center',
-      ...style
-    }} {...props} />
-  ))
-
-  return { Modal, animate }
+type Props = MotionProps & { opacity?: number }
+/** Presence-based, controlled modal (simple wrapper around `<Modal>`) */
+export function useModal(props: Props): Hook {
+  const [show, setShow] = useState(false)
+  return { Modal: <Modal show={show} {...props} />, animate: setShow }
 }
